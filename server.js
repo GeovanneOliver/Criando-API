e78bf8941309ee9253ef9f1efb002593feabb386
GeovanneOliver <geo-san@hotmail.com>
2024-10-13 11:07:43 -0300

    Commit de finalização da aula 2

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 import { log } from "console";
-import http from "http"; //nativa do node
+//import http from "http"; //http nativa do node
+import app from "./src/app.js"
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores",
-};
+// const rotas = {
+//     "/": "Curso de Node.js",
+//     "/livros": "Entrei na rota livros",
+//     "/autores": "Entrei na rota autores",
+// };
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-});
+//  const server = http.createServer((req, res) => {
+//     res.writeHead(200, {"Content-Type": "text/plain" });
+//     res.end(rotas[req.url]);
+// }); 
 
-server.listen(PORT, () => {
-    console.log("Servidor escutando!");   
+app.listen(PORT, () => {
+    console.log("Servidor escutando!");
 })
